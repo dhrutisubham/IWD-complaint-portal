369a311feb85fcc278a04a180269bae47d9903e2
dhrutisubham <dhruti.subham@gmail.com>
2024-04-12 18:38:46 +0530

    changes in db.js

--- core/db.js
+++ core/db.js
@@ -3,7 +3,7 @@ import logger from './logger.js';
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
-    storage: '../db/database.sqlite',
+    storage: './db/database.sqlite',
     logging: (msg) => {
         logger.info(msg); // Pass the log message to your Pino logger
     }
